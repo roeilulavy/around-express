1268762234569fb339d36402bf2e42736b7f9204
Roy Lulavy <49700286+roeilulavy@users.noreply.github.com>
2022-02-21 20:35:54 +0200

    .eslintrc - console.log

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const helmet = require("helmet");
+const helmet = require('helmet');
 
 const app = express();
 app.use(helmet());
@@ -9,11 +9,11 @@ const { PORT = 3000 } = process.env;
 const userRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
 
-app.use("/users", userRouter);
-app.use("/cards", cardsRouter);
+app.use('/users', userRouter);
+app.use('/cards', cardsRouter);
 
 app.get('/*', (req, res) => {
-  res.status(404).send({ "message": "Requested resource not found" });
+  res.status(404).send({ 'message': 'Requested resource not found' });
 });
 
 app.listen(PORT, () => {
