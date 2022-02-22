8efd2a669522e2304d1ebfc1154bef98d2dba47c
Roy Lulavy <49700286+roeilulavy@users.noreply.github.com>
2022-02-22 12:27:29 +0200

    to delete

--- helpers/files.js
+++ helpers/files.js
@@ -1,11 +1,12 @@
+const { status } = require('express/lib/response');
 const fs = require('fs')
 
 const getJsonFromFile = (filePath) => {
   return fs.readFile(filePath, { encoding: 'utf8' }, (err, data) => {
     if (err) {
           console.log(err); // eslint-disable-line no-console
-          res.status(500).send({ message: 'An error has occurred on the server' });
-          return;
+
+          return({ message: 'An error has occurred on the server' });
         }
         return JSON.parse(data);
   });
--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,26 @@
 const express = require('express');
+const path = require('path');
 const { getJsonFromFile } = require('../helpers/files');
 
 const router = express.Router();
+const usersPath = path.join(__dirname, '..', 'data', 'users.json');
 
 router.get('/', (req, res) => {
-  const users = getJsonFromFile('../data/users.json');
+
+  const users = getJsonFromFile(usersPath);
   res.send(users);
 });
 
 router.get('/:id', (req, res) => {
   const id = req.params;
-  const usersList = getJsonFromFile('../data/users.json');
+  const usersList = getJsonFromFile(usersPath);
   const user = usersList.find((users) => users._id === id);
   if (user) {
     res.send(user);
   } else {
     res.status(404).send({ message: 'User ID not found' });
   }
+  console.log(user);
 });
 
 module.exports = router;
