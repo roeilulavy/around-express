a1db17dea6555ba6ebfd14d007599a83a3699e7c
Roy Lulavy <49700286+roeilulavy@users.noreply.github.com>
2022-02-22 12:32:07 +0200

    Revert "lint fix"
    This reverts commit 2e215272b15bc80e073826110980955e3d02facb.

--- routes/cards.js
+++ routes/cards.js
@@ -1,16 +1,15 @@
 const express = require('express');
+const router = express.Router();
 const fs = require('fs');
 const path = require('path');
 
-const router = express.Router();
-
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   const dataPath = path.join(__dirname, '../data/cards.json');
 
-  fs.readFile(dataPath, { encoding: 'utf8' }, (err, data) => {
-    if (err) {
-      console.log(err); // eslint-disable-line no-console
-      res.status(500).send({ message: 'An error has occurred on the server' });
+  fs.readFile(dataPath, {encoding: 'utf8'}, (err, data) => {
+    if(err) {
+      console.log(err);
+      res.status(500).send({ "message": "An error has occurred on the server" });
       return;
     }
     const cards = JSON.parse(data);
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,13 @@
 const express = require('express');
+<<<<<<< HEAD
+=======
+const router = express.Router();
+const fs = require('fs');
+>>>>>>> parent of 2e21527 (lint fix)
 const path = require('path');
 const { getJsonFromFile } = require('../helpers/files');
 
+<<<<<<< HEAD
 const router = express.Router();
 const usersPath = path.join(__dirname, '..', 'data', 'users.json');
 
@@ -21,6 +27,40 @@ router.get('/:id', (req, res) => {
     res.status(404).send({ message: 'User ID not found' });
   }
   console.log(user);
+=======
+router.get("/", (req, res) => {
+  const dataPath = path.join(__dirname, '../data/users.json');
+
+  fs.readFile(dataPath, {encoding: 'utf8'}, (err, data) => {
+    if(err) {
+      console.log(err);
+      res.status(500).send({ "message": "An error has occurred on the server" });
+      return;
+    }
+    const users = JSON.parse(data);
+    res.send(users);
+  });
+});
+
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  const dataPath = path.join(__dirname, '../data/users.json');
+
+  fs.readFile(dataPath, {encoding: 'utf8'}, (err, data) => {
+    if(err) {
+      console.log(err);
+      res.status(500).send({ "message": "An error has occurred on the server" });
+      return;
+    }
+    const usersList = JSON.parse(data);
+    const user = usersList.find((users) => users._id === id);
+    if (user) {
+      res.send(user);
+    } else {
+      res.status(404).send({ "message": "User ID not found" });
+    }
+  });
+>>>>>>> parent of 2e21527 (lint fix)
 });
 
 module.exports = router;
