1c4925c466cf5936a8a5178b1ae0d3cb8fcad2a6
Roy Lulavy <49700286+roeilulavy@users.noreply.github.com>
2022-02-22 12:57:55 +0200

    Start from finish

--- routes/users.js
+++ routes/users.js
@@ -1,52 +1,16 @@
 const express = require('express');
-const router = express.Router();
 const fs = require('fs');
 const path = require('path');
-const { getJsonFromFile } = require('../helpers/files');
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 const router = express.Router();
-const usersPath = path.join(__dirname, '..', 'data', 'users.json');
 
 router.get('/', (req, res) => {
-
-  const users = getJsonFromFile(usersPath);
-  res.send(users);
-});
-
-router.get('/:id', (req, res) => {
-  const id = req.params;
-  const usersList = getJsonFromFile(usersPath);
-  const user = usersList.find((users) => users._id === id);
-  if (user) {
-    res.send(user);
-  } else {
-    res.status(404).send({ message: 'User ID not found' });
-  }
-  console.log(user);
-
-  router.get("/", (req, res) => {
-=======
-router.get("/", (req, res) => {
->>>>>>> parent of 2e21527 (lint fix)
-=======
-router.get("/", (req, res) => {
->>>>>>> parent of 2e21527 (lint fix)
-=======
-router.get("/", (req, res) => {
->>>>>>> parent of 2e21527 (lint fix)
-=======
-router.get("/", (req, res) => {
->>>>>>> parent of 2e21527 (lint fix)
   const dataPath = path.join(__dirname, '../data/users.json');
 
-  fs.readFile(dataPath, {encoding: 'utf8'}, (err, data) => {
-    if(err) {
-      console.log(err);
-      res.status(500).send({ "message": "An error has occurred on the server" });
+  fs.readFile(dataPath, { encoding: 'utf8' }, (err, data) => {
+    if (err) {
+      console.log(err); // eslint-disable-line no-console
+      res.status(500).send({ message: 'An error has occurred on the server' });
       return;
     }
     const users = JSON.parse(data);
@@ -54,14 +18,14 @@ router.get("/", (req, res) => {
   });
 });
 
-router.get("/:id", (req, res) => {
-  const id = req.params.id;
+router.get('/:id', (req, res) => {
+  const id = req.params;
   const dataPath = path.join(__dirname, '../data/users.json');
 
-  fs.readFile(dataPath, {encoding: 'utf8'}, (err, data) => {
-    if(err) {
-      console.log(err);
-      res.status(500).send({ "message": "An error has occurred on the server" });
+  fs.readFile(dataPath, { encoding: 'utf8' }, (err, data) => {
+    if (err) {
+      console.log(err); // eslint-disable-line no-console
+      res.status(500).send({ message: 'An error has occurred on the server' });
       return;
     }
     const usersList = JSON.parse(data);
@@ -69,7 +33,7 @@ router.get("/:id", (req, res) => {
     if (user) {
       res.send(user);
     } else {
-      res.status(404).send({ "message": "User ID not found" });
+      res.status(404).send({ message: 'User ID not found' });
     }
   });
 });
