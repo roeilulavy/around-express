55227f22d7a5e0d8130d493fa1d7c2afb94d302a
Roy Lulavy <49700286+roeilulavy@users.noreply.github.com>
2022-02-22 12:43:21 +0200

    Revert "lint fix"
    This reverts commit 2e215272b15bc80e073826110980955e3d02facb.

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,7 @@ const { getJsonFromFile } = require('../helpers/files');
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 const router = express.Router();
 const usersPath = path.join(__dirname, '..', 'data', 'users.json');
 
@@ -27,6 +28,9 @@ router.get('/:id', (req, res) => {
   console.log(user);
 
   router.get("/", (req, res) => {
+=======
+router.get("/", (req, res) => {
+>>>>>>> parent of 2e21527 (lint fix)
 =======
 router.get("/", (req, res) => {
 >>>>>>> parent of 2e21527 (lint fix)
