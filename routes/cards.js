c7656dbec7a57782a8ec5842490f357e5995d618
Roy Lulavy <49700286+roeilulavy@users.noreply.github.com>
2022-02-22 12:31:22 +0200

    Revert "Add helper read Json from file"
    This reverts commit 63e2abaceeb41306ff364de437d2388e4c85834b.

--- routes/cards.js
+++ routes/cards.js
@@ -1,11 +1,21 @@
 const express = require('express');
-const { getJsonFromFile } = require('../helpers/files');
+const fs = require('fs');
+const path = require('path');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  const cards = getJsonFromFile('../data/cards.json');
-  res.send(cards);
+  const dataPath = path.join(__dirname, '../data/cards.json');
+
+  fs.readFile(dataPath, { encoding: 'utf8' }, (err, data) => {
+    if (err) {
+      console.log(err); // eslint-disable-line no-console
+      res.status(500).send({ message: 'An error has occurred on the server' });
+      return;
+    }
+    const cards = JSON.parse(data);
+    res.send(cards);
+  });
 });
 
 module.exports = router;
