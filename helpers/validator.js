2ada40774942637bbe775feb910fb3c5e3eae898
Roy Lulavy <49700286+roeilulavy@users.noreply.github.com>
2022-04-07 23:06:45 +0300

    validator.js

--- helpers/validator.js
+++ helpers/validator.js
@@ -1,10 +1,10 @@
 const validator = require('validator');
 
-const validateURL = (value, helpers) => {
+const validateUrl = (value, helpers) => {
   if (validator.isURL(value, { require_protocol: true, allow_underscores: true })) {
     return value;
   }
   return helpers.error('string.uri');
 };
 
-module.exports = { validateURL };
+module.exports = { validateUrl };
