7783ddfa3d67692befa8874fd61ccd8eaa95e7d3
Roy Lulavy <49700286+roeilulavy@users.noreply.github.com>
2022-04-13 16:18:57 +0300

    console.log(card);

--- controllers/cards.js
+++ controllers/cards.js
@@ -45,6 +45,7 @@ module.exports.deleteCard = async (req, res, next) => {
   const { _id } = req.user;
   try {
     const card = await Card.findById(id);
+    console.log(card);
     const cardOwner = card.owner.toHexString();
     if (_id === cardOwner) {
       const deleteCard = await Card.findByIdAndRemove(id);
