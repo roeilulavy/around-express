ddd489f4a5fd9bb47622c935f7f2c86b3258bc23
Roy Lulavy <49700286+roeilulavy@users.noreply.github.com>
2022-04-15 13:10:05 +0300

    7

--- middleware/handleErrors.js
+++ middleware/handleErrors.js
@@ -1,15 +1,11 @@
 const handleErrors = (err, req, res, next) => {
-  console.log('handleError console');
-  console.log(err);
-
   if (err.statusCode && err.message) {
     res.status(err.statusCode).send({ message: err.message });
     next();
     return;
   }
 
-  // res.status(500).send({ message: `some thing went wrong with the server:  ${err.message}` });
-  res.status(201).send({ message: `some thing went wrong with the server:  ${err.message}` });
+  res.status(500).send({ message: `some thing went wrong with the server:  ${err.message}` });
 
   next();
 };
