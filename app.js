40bab74101dd7f2dfaf6a192aa61132d52aaa116
Roy Lulavy <49700286+roeilulavy@users.noreply.github.com>
2022-04-15 13:50:26 +0300

    routers back to app.js

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
-const bodyParser = require('body-parser');
-const { errors } = require('celebrate');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const rateLimiter = require('express-rate-limit');
-const routers = require('./routes/routers');
+const { errors } = require('celebrate');
+const { celebrate, Joi } = require('celebrate');
+const { handleCors } = require('./middleware/cors');
 const { handleErrors } = require('./middleware/handleErrors');
 const { requestLogger, errorLogger } = require('./middleware/logger');
+const { login, createUser } = require('./controllers/users');
+const { NotFoundError } = require('./utils/errorHandler');
+const { auth } = require('./middleware/auth');
+const usersRouter = require('./routes/users');
+const cardsRouter = require('./routes/cards');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 const limiter = rateLimiter({
   windowMs: 15 * 60 * 1000,
-  max: 10000,
+  max: 500,
   standardHeaders: true,
   legacyHeaders: false,
 });
@@ -26,18 +32,37 @@ app.use(helmet());
 app.use(bodyParser.json());
 app.disable('x-powered-by');
 
+app.use(handleCors);
 app.use(cors());
 app.options('*', cors());
 
 app.use(requestLogger);
 
 // routes
-app.use('/', routers);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(6),
+  }),
+}), login);
+
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(6),
+  }).unknown(true),
+}), createUser);
+
+app.use('/users', auth, usersRouter);
+app.use('/cards', auth, cardsRouter);
+
+app.get('/*', (req, res, next) => {
+  next(new NotFoundError('Requested resource not found'));
+});
 
 app.use(errorLogger);
 app.use(handleErrors);
-
-routers.use(errors());
+app.use(errors());
 
 mongoose.connection.once('error', () => {
   console.error.bind(console, 'MongoDB Connection Error: ');// eslint-disable-line no-console
